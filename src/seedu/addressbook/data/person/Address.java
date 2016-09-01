591aade537a094136d58fcc27492105313ad93e8
woo hian chua <woo hian chua>
2016-09-01 17:02:11 +0800

    Added Class for block, street, unit, postal code

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -11,8 +11,14 @@ public class Address {
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String ADDRESS_FORMAT = ", ";
 
     public final String value;
+    public String[] address;
+    public Block block;
+    public Street street;
+    public Unit unit;
+    public PostalCode postalCode;
     private boolean isPrivate;
 
     /**
@@ -25,8 +31,15 @@ public class Address {
         if (!isValidAddress(address)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = address;
+        
+        value = address;
+        this.address = address.split(", ");
+        block = new Block(this.address[0]);
+        street = new Street(this.address[1]);
+        unit = new Unit(this.address[2]);
+        postalCode = new PostalCode(this.address[3]);        
     }
+    
 
     /**
      * Returns true if a given string is a valid person email.
@@ -37,7 +50,8 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return block.getBlock() + ADDRESS_FORMAT + street.getStreet() + ADDRESS_FORMAT +
+        		unit.getUnit() + ADDRESS_FORMAT + postalCode.getPostalCode();
     }
 
     @Override
@@ -55,4 +69,52 @@ public class Address {
     public boolean isPrivate() {
         return isPrivate;
     }
+}
+
+class Block{
+	private String block;
+	
+	public Block(String block){
+		this.block = block;
+	}
+	
+	public String getBlock(){
+		return block;
+	}
+}
+
+class Street{
+	private String street;
+	
+	public Street(String street){
+		this.street = street;
+	}
+	
+	public String getStreet(){
+		return street;
+	}
+}
+
+class Unit{
+	private String unit;
+	
+	public Unit(String unit){
+		this.unit = unit;
+	}
+	
+	public String getUnit(){
+		return unit;
+	}
+}
+
+class PostalCode{
+	private String postal;
+	
+	public PostalCode(String postal){
+		this.postal = postal;
+	}
+	
+	public String getPostalCode(){
+		return postal;
+	}
 }
